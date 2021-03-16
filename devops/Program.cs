5e6cfcfe519918ae12f4c7e50df8f7cda6ee19da
Petre Claudiu <petre.claudiu.benone@outlook.com>
2021-03-16 16:49:49 +0200

    Comments made

--- devops/Program.cs
+++ devops/Program.cs
@@ -15,7 +15,7 @@ namespace devops
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+        //Changes made
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
